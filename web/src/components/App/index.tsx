ce602f975a0f6c1e7cbf221d4adecbfbe6d6b4af
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-13 14:55:21 -0400

    :fire: add tailwind and confi bg on index.html

--- web/src/components/App/index.tsx
+++ web/src/components/App/index.tsx
@@ -1,6 +1,8 @@
+import "../../styles/main.css";
+
 export const App = () => {
   return (
-    <div className="App">
+    <div className="w-8 h-8 bg-black lg:bg-violet-500">
       <h1>hello world</h1>
     </div>
   );
