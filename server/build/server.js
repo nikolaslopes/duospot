d40ac3b6a5904a17d8537ecfb8af2270d390633a
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-13 01:54:57 -0400

    :fire: add tsx

---
+++ server/build/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+app.get('/ads', (request, response) => {
+    console.log('access');
+    return response.json([
+        {
+            id: 1,
+            name: 'Nikolas',
+        },
+    ]);
+});
+app.listen(3333);
