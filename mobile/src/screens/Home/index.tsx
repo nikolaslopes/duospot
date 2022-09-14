a94d83e395f666b6691c94fa50c27d8079ba5e79
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-14 19:27:27 -0400

    :sparkles: created FlatList

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { View, Image, FlatList } from "react-native";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { GameCard } from "../../components/GameCard";
@@ -16,7 +16,14 @@ export const Home = () => {
         subtitle="Selecione o game que deseja jogar..."
       />
 
-      <GameCard data={GAMES[0]} />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
     </View>
   );
 };
