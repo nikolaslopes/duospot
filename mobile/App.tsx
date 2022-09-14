ceb313bc5b9da75efa64b4ff955ca2f76f92a45d
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-14 17:36:05 -0400

    :fire: created mobile bg

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,41 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Background } from "./src/components/Background";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>pode react native no grupo?😔</Text>
-      <Button label="click me" />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Background></Background>;
 }
-
-interface ButtonProps {
-  label: string;
-}
-
-const Button = ({ label }: ButtonProps) => {
-  return (
-    <TouchableOpacity>
-      <Text style={styles.buttonTitle}>{label}</Text>
-    </TouchableOpacity>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#040404",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 24,
-    color: "#fcfcfc",
-  },
-  buttonTitle: {
-    fontSize: 16,
-    color: "#ccc",
-  },
-});
