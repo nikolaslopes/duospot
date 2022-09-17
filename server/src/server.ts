1ea2406b8e418925cfba657933b03bcdabf3850d
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-17 02:01:29 -0400

    :fire: add prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,21 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
 
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
 app.get("/games/:id/ads", (request, response) => {
-  const gameId = request.params.id;
+  return response.json([]);
+});
 
-  return response.send(gameId);
-
-  return response.json([
-    {
-      id: 1,
-      name: "Nikolas",
-    },
-    {
-      id: 2,
-      name: "Kiki",
-    },
-    {
-      id: 3,
-      name: "Zuzu",
-    },
-  ]);
+app.get("/ads/:id/discord", (request, response) => {
+  return response.json([]);
 });
 
 app.listen(3333);
