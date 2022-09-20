d20644b93a981bc8a053c2cdf47490f5e3bc413a
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-20 03:08:49 -0400

    :dizzy: config gsap animations

--- web/src/components/GameBanner/index.tsx
+++ web/src/components/GameBanner/index.tsx
@@ -1,8 +1,50 @@
+import {
+  ChangeEvent,
+  HtmlHTMLAttributes,
+  MouseEvent,
+  MouseEventHandler,
+  useEffect,
+  useRef,
+} from "react";
+import { gsap } from "gsap";
+
 import { GameBannerProps } from "../../types/gameBanner";
 
 export const GameBanner = ({ bannerUrl, title, adsCount }: GameBannerProps) => {
+  const boxRef = useRef();
+
+  useEffect(() => {
+    const boxes = [boxRef.current];
+
+    gsap.fromTo(
+      boxes,
+      {
+        scale: 0,
+      },
+      {
+        scale: 1,
+        duration: 1.2,
+        ease: "bounce",
+      }
+    );
+  }, []);
+
+  const onEnter = ({ currentTarget }: MouseEvent<HTMLAnchorElement>) => {
+    gsap.to(currentTarget, { scale: 1.1 });
+  };
+
+  const onLeave = ({ currentTarget }: MouseEvent<HTMLAnchorElement>) => {
+    gsap.to(currentTarget, { scale: 1 });
+  };
+
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <a
+      href="#"
+      className="relative rounded-lg overflow-hidden cursor-pointer"
+      ref={boxRef}
+      onMouseEnter={onEnter}
+      onMouseLeave={onLeave}
+    >
       <img src={bannerUrl} alt="" />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0">
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,15 +2,20 @@ import { GameBanner } from "../../components/GameBanner";
 import { CreateNewAdBanner } from "../../components/CreateNewAdBanner";
 import { useGamesData } from "../../hooks/useGames";
 
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import logoImg from "../../assets/logo.svg";
 
 export const Home = () => {
   const games = useGamesData();
   const data = games.data;
 
+  const [animationParent] = useAutoAnimate({
+    duration: 1000,
+  });
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="logo" />
+      <img src={logoImg} alt="logo" className="animate-bounce" />
 
       <h1 className="text-6xl text-white font-black mt-20">
         Seu{" "}
