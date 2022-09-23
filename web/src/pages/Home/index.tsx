49790657bfaa022ac233f354eca0b4e026add075
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-22 20:24:48 -0400

    :fire: form created

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -48,65 +48,63 @@ export const Home = () => {
             <Dialog.Title className="text-3xl font-black">
               Publique um anúncio
             </Dialog.Title>
-            <Dialog.Content>
-              <form>
-                <div>
-                  <label htmlFor="game">Qual o game?</label>
-                  <input
-                    id="game"
-                    placeholder="Selecione o game que deseja jogar"
-                  />
-                </div>
+            <form className="mt-8">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="game" className="font-semibold">
+                  Qual o game?
+                </label>
+                <input
+                  id="game"
+                  placeholder="Selecione o game que deseja jogar"
+                  className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
+                />
+              </div>
 
-                <div>
-                  <label htmlFor="name">Seu nome (ou nickname)</label>
-                  <input
-                    id="game"
-                    placeholder="Como te chamam dentro do game?"
-                  />
-                </div>
+              <div>
+                <label htmlFor="name">Seu nome (ou nickname)</label>
+                <input id="game" placeholder="Como te chamam dentro do game?" />
+              </div>
 
-                <div>
-                  <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                  <input
-                    id="yearsPlaying"
-                    type="number"
-                    placeholder="Tudo bem ser ZERO"
-                  />
-                </div>
+              <div>
+                <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                <input
+                  id="yearsPlaying"
+                  type="number"
+                  placeholder="Tudo bem ser ZERO"
+                />
+              </div>
 
-                <div>
-                  <label htmlFor="discord">Qual seu Discord?</label>
-                  <input id="discord" type="text" placeholder="Usuario#000" />
-                </div>
+              <div>
+                <label htmlFor="discord">Qual seu Discord?</label>
+                <input id="discord" type="text" placeholder="Usuario#000" />
+              </div>
 
+              <div>
                 <div>
+                  <label htmlFor="weekDays">Quando costuma jogar?</label>
+                </div>
+                <div>
+                  <label htmlFor="hourStart">Qual horário do dia?</label>
                   <div>
-                    <label htmlFor="weekDays">Quando costuma jogar?</label>
-                  </div>
-                  <div>
-                    <label htmlFor="hourStart">Qual horário do dia?</label>
-                    <div>
-                      <input id="hourStart" type="time" placeholder="De" />
-                      <input id="hourEnd" type="time" placeholder="Até" />
-                    </div>
+                    <input id="hourStart" type="time" placeholder="De" />
+                    <input id="hourEnd" type="time" placeholder="Até" />
                   </div>
                 </div>
+              </div>
 
-                <div>
-                  <input type="checkbox" />
-                  Costumo me conectar ao chat de voz
-                </div>
+              <div>
+                <input type="checkbox" />
+                Costumo me conectar ao chat de voz
+              </div>
 
-                <footer>
-                  <button>Cancelar</button>
-                  <button type="submit">
-                    {" "}
-                    <GameController /> Encontrar duo
-                  </button>
-                </footer>
-              </form>
-            </Dialog.Content>
+              <footer>
+                <button>Cancelar</button>
+                <button type="submit">
+                  {" "}
+                  <GameController /> Encontrar duo
+                </button>
+              </footer>
+            </form>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
