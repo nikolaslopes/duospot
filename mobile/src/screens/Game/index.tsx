e3e57ebfdac166ae92225cfb8af18c9d0380eeb4
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-25 02:32:28 -0400

    :fire: created header, logo, container and fragmentRigh styles

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,20 +1,36 @@
 import { useRoute } from "@react-navigation/native";
-import { Text } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Entypo } from "@expo/vector-icons";
 
 import { IGame } from "../../types/game";
+
 import { Background } from "../../components/Background";
+
+import { THEME } from "../../assets/theme";
+import { styles } from "./styles";
+import logoImg from "../../assets/images/logo-nlw-esports.png";
 
 export const Game = () => {
   const route = useRoute();
   const game = route.params as IGame;
 
-  console.log(game);
-
   return (
     <Background>
-      <SafeAreaView>
-        <Text>hi</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} style={styles.logo} />
+
+          <View style={styles.fragmentRight} />
+        </View>
       </SafeAreaView>
     </Background>
   );
