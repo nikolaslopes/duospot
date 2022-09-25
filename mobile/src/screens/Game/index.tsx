84fe2bf133b59c41df5ddf7791d8024c6e865506
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-25 03:40:36 -0400

    :fire: created game route

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,25 +1,32 @@
-import { useRoute } from "@react-navigation/native";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Image, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Entypo } from "@expo/vector-icons";
 
-import { IGame } from "../../types/game";
-
 import { Background } from "../../components/Background";
+import { Heading } from "../../components/Heading";
+
+import { IGame } from "../../types/game";
 
 import { THEME } from "../../assets/theme";
 import { styles } from "./styles";
 import logoImg from "../../assets/images/logo-nlw-esports.png";
 
 export const Game = () => {
+  const navigation = useNavigation();
+
   const route = useRoute();
   const game = route.params as IGame;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleGoBack}>
             <Entypo
               name="chevron-thin-left"
               color={THEME.COLORS.CAPTION_300}
@@ -31,6 +38,14 @@ export const Game = () => {
 
           <View style={styles.fragmentRight} />
         </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
       </SafeAreaView>
     </Background>
   );
