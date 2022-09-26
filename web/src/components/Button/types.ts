5d5892feac19ede0ea7366ad5b7ba1c3b2bdf0b6
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-25 21:42:53 -0400

    :fire: refactor web tree to a new pattern

--- web/src/components/Button/types.ts
+++ web/src/components/Button/types.ts
@@ -1,6 +1,7 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
-  icon?: ReactNode;
   title: string;
+  variant: string;
+  icon?: ReactNode;
 }
